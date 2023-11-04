28602704c9c614ef0a3feac0967f43a1b8475456
Zuegelwagen <m.inglin@gmx.ch>
2023-11-04 23:38:53 +0100

    Function timeOfDay added

--- js/summary.js
+++ js/summary.js
@@ -1,3 +1,32 @@
+function init() {
+  getTimeOfDay();
+}
+
+function getTimeOfDay() {
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+  let timeOfDay;
+
+  if (currentHour >= 5 && currentHour < 12) {
+    timeOfDay = "morning";
+  } else if (currentHour >= 12 && currentHour < 18) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "evening";
+  }
+  createGreeting(timeOfDay);
+}
+
+function createGreeting(timeOfDay) {
+  let greetingContainer = document.getElementById("greetingContainer");
+  greetingContainer.innerHTML = "";
+  /*html*/
+  greetingContainer.innerHTML = `
+    <p class="welcome-text">Good ${timeOfDay},</p>
+    <p class="user-name">Sofia Müller</p>
+    `;
+}
+
 function changeCirleDark() {
   const circleImage = document.getElementById("checkedCircle");
   const newImageSrc = "./img/checked_circle_dark.svg";
@@ -6,22 +35,22 @@ function changeCirleDark() {
 }
 
 function changeCirleFair() {
-    const circleImage = document.getElementById("checkedCircle");
-    const newImageSrc = "./img/checked_circle.svg";
+  const circleImage = document.getElementById("checkedCircle");
+  const newImageSrc = "./img/checked_circle.svg";
 
-    circleImage.src = newImageSrc;
+  circleImage.src = newImageSrc;
 }
 
 function changePencilDark() {
-    const circleImage = document.getElementById("pencilCircle");
-    const newImageSrc = "./img/pencil_circle_dark.svg";
-  
-    circleImage.src = newImageSrc;
+  const circleImage = document.getElementById("pencilCircle");
+  const newImageSrc = "./img/pencil_circle_dark.svg";
+
+  circleImage.src = newImageSrc;
 }
 
 function changePencilFair() {
-    const circleImage = document.getElementById("pencilCircle");
-    const newImageSrc = "./img/pencil_circle.svg";
-  
-    circleImage.src = newImageSrc;
+  const circleImage = document.getElementById("pencilCircle");
+  const newImageSrc = "./img/pencil_circle.svg";
+
+  circleImage.src = newImageSrc;
 }
