2061370c20ea0d371a9df3f1e91a20c9ce495d40
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2023-11-13 01:16:40 +0100

    update some functions on addtask

--- js/addtask.js
+++ js/addtask.js
@@ -1,5 +1,6 @@
 let priorityOfTask;
-subtasks = [];
+let subtasks = [];
+let selectedContacts = [];
 
 function initAddTask() {
     setIdOfCurrentPage(1);
@@ -22,17 +23,17 @@ function renderContacts(content) {
     for (let i = 0; i < contacts.length; i++) {
         const contact = contacts[i];
         content.innerHTML += `
-        <div id="contact${i}" class="contact">
+        <div id="contact${contact.id}" class="contact"onclick="chooseContactToAssign(${contact.id}); doNotTriggerEvent(event)">
             <div class="allign-center">
                 <div class="pos-rel">
                     <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle id="Ellipse 5" cx="21" cy="21" r="20" fill="${contact.icon}" stroke="white" stroke-width="2"/>
                     </svg>
-                    <p class="initialTag">SM</p>
+                    <p class="initialTag">${contact.firstname.charAt(0)}${contact.lastname.charAt(0)}</p>
                 </div>
                 ${contact.firstname} ${contact.lastname}
             </div>
-            <img src="../img/add_task/Check button.png">
+            <img src="../img/add_task/Check button.png" id="checkContact${contact.id}">
         </div>
     `;
     };
@@ -169,6 +170,7 @@ function clearAll() {
     document.getElementById('inputSubTask').value = '';
     subtasks.splice(0, subtasks.length);
     renderSubtasks();
+    renderContactInitialIcons();
 }
 
 function editSubtask(id, btn1, btn2, i) {
@@ -217,6 +219,7 @@ function openContactList() {
     openContactsDropDown.style.transform = 'rotate(180deg)';
     setTimeout(() => {
         contactList.setAttribute('onclick', 'closeContactList()');
+        openContactsDropDown.setAttribute('onclick', 'closeContactList()');
     }, 100);
 }
 
@@ -226,6 +229,69 @@ function closeContactList() {
     contactList.style.height = '50px';
     openContactsDropDown.style.transform = 'rotate(0deg)';
     setTimeout(() => {
-        contactList.setAttribute('onclick', 'openContactList()');
+        contactList.setAttribute('onclick', 'openContactList(); doNotTriggerEvent(event)');
+        openContactsDropDown.setAttribute('onclick', 'openContactList(); doNotTriggerEvent(event)');
     }, 100);
+}
+
+function searchContactToAssign() {
+    let contactsDropDown = document.getElementById('contactsDropDown');
+    let input = document.getElementById('contactInput').value.toLowerCase();
+    contactsDropDown.innerHTML = '';
+    let matchingContacts = contacts.filter(contact => 
+        contact.firstname.toLowerCase().includes(input) || contact.lastname.toLowerCase().includes(input)
+    );
+    matchingContacts.forEach((contact) => {
+        contactsDropDown.innerHTML += `
+        <div id="contact${contact.id}" class="contact" onclick="chooseContactToAssign(${contact.id}); doNotTriggerEvent(event)">
+            <div class="allign-center">
+                <div class="pos-rel">
+                    <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle id="Ellipse 5" cx="21" cy="21" r="20" fill="${contact.icon}" stroke="white" stroke-width="2"/>
+                    </svg>
+                    <p class="initialTag">${contact.firstname.charAt(0)}${contact.lastname.charAt(0)}</p>
+                </div>
+                ${contact.firstname} ${contact.lastname}
+            </div>
+            <img src="../img/add_task/Check button.png" id="checkContact${contact.id}">
+        </div>
+        `;
+    })
+}
+
+function chooseContactToAssign(id) {
+    let checkImg = document.getElementById(`checkContact${id}`);
+    let contact = document.getElementById(`contact${id}`);
+    checkImg.setAttribute('src', '../img/add_task/checked.png');
+    contact.setAttribute('onclick', `unchooseContact(${id}); doNotTriggerEvent(event)`);
+    contact.classList.add('active-contact');
+    selectedContacts.push(id);
+    renderContactInitialIcons();
+}
+
+function unchooseContact(id) {
+    let checkImg = document.getElementById(`checkContact${id}`);
+    let contact = document.getElementById(`contact${id}`);
+    checkImg.setAttribute('src', '../img/add_task/Check button.png');
+    contact.setAttribute('onclick', `chooseContactToAssign(${id}); doNotTriggerEvent(event)`);
+    contact.classList.remove('active-contact');
+    let i = selectedContacts.indexOf(id);
+    selectedContacts.splice(i, 1);
+    renderContactInitialIcons();
+}
+
+function renderContactInitialIcons() {
+    let contactInitialIcons = document.getElementById('contactInitialIcons');
+    contactInitialIcons.innerHTML = '';
+    selectedContacts.forEach((id) => {
+        let contact = contacts[id]
+        contactInitialIcons.innerHTML += `
+            <div class="selected-initial-contact">
+                <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle id="Ellipse 5" cx="21" cy="21" r="20" fill="${contact.icon}" stroke="white" stroke-width="2"/>
+                </svg>
+                <p class="selected-initial-tag">${contact.firstname.charAt(0)}${contact.lastname.charAt(0)}</p>
+            </div>
+        `;
+    })
 }
