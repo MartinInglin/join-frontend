b2ef9f0591feead9ac4dc857b0d7f0c17a8fc751
ErikMic <erikmic@hotmail.de>
2023-11-19 00:02:56 +0100

    update contact

--- js/contacts.js
+++ js/contacts.js
@@ -82,6 +82,79 @@ function hideContact() {
 }
 
 function editContact(id) {
+    showAddNewContact();
+    let bgMessage = document.getElementById('bg-message');        
+    bgMessage.classList.remove('d-none');
+
+    let addNewContact = document.getElementById('add-new-contact');
+    let contact = contacts[id];
+
+    addNewContact.innerHTML = 
+    /*html*/`
+    <div class="add-new-cont-left">
+        <div class="icon"> 
+            <img src="img/addnewcontact/icon.svg">
+        </div>
+        <div class="headline-add-new-cont">
+            <p>Edit contact</p>
+        </div>
+        <img src="img/addnewcontact/underline.svg">
+    </div>    
+    <div onclick="closeAddNewContact()" class="close-btn">
+        <img src="img/addnewcontact/close.svg">
+    </div>
+    <div class="cont-icon-big circle">
+        <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="50" cy="50" r="50" fill="white" />
+            <circle cx="50" cy="50" r="46" fill="${contact['icon']}" />
+            <text x="30" y="58" font-size="24" fill="white">${contact['firstname'].charAt(0)}</text>
+            <text x="50" y="58" font-size="24" fill="white">${contact['lastname'].charAt(0)}</text>                    
+        </svg>
+    </div>
+    <form class="info-new-cont" onsubmit="editOldContact(id); return false;">
+        <div class="input-field">
+            <input id="edit-name" type="text" value="${contact['firstname']} ${contact['lastname']}" pattern="[A-Z]{1}[a-z]{2,} [A-Z]{1}[a-z]{2,}" title="Capitalise the first letter e.g. Max Musterman" placeholder="Name" class="form-control" required>
+            <span class="info-icon icon-name"></span>
+        </div>
+        <div class="input-field"> 
+            <input id="edit-email" type="email" value="${contact['email']}" pattern="^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$" title="e.g. maxmustermann@hotmail.de" placeholder="Email" class="form-control" required>
+            <span class="info-icon icon-email"></span>
+        </div>
+        <div class="input-field">
+            <input id="edit-phone" type="tel" value="${contact['phone-number']}" pattern="[+][0-9]{2} [0-9]{3}-[0-9]{4,}" title="e.g. +49 123-123456789" placeholder="Phone" class="form-control" required>
+            <span class="info-icon icon-phone"></span>
+        </div>
+        <div class="btn-cancel-create">
+            <button id="btn-delete" onclick="deleteContact(id)">
+                Delete
+            </button>
+            <button id="btn-save" type="submit">
+                Save
+                <img src="../img/add_task/check2.png" alt>
+            </button>
+        </div>
+    </form>
+    `;
+}
+
+function editOldContact(id) {
+    let contact = contacts[id];
+    let nameInput = document.getElementById('edit-name');
+    let emailInput = document.getElementById('edit-email');
+    let phoneInput = document.getElementById('edit-phone');
+    let nameParts = nameInput.value.split(' ');
+    let firstname = nameParts[0] || '';
+    let lastname = nameParts.slice(1).join(' ') || '';
+
+    contact.icon = contact.icon;
+    contact.firstname = firstname;
+    contact.lastname = lastname;
+    contact.email = emailInput.value;
+    contact['phone-number'] = phoneInput.value;
+
+    setContacts();
+    closeAddNewContact();
+    showContact(contact.id);
 }
 
 async function deleteContact(id) {
