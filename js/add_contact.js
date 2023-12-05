c57e21e0de2dc7af00d010c05061172e49c0bd6e
Zuegelwagen <m.inglin@gmx.ch>
2023-12-05 22:24:49 +0100

    add contact debugger removed

--- js/add_contact.js
+++ js/add_contact.js
@@ -133,7 +133,6 @@ function createNewContact() {
  * @param {string} newContact ['phone-number'] - The phone number of the contact.
  */
 function pushNewContact(newContact) {
-    debugger
     contacts.push(newContact);
     setContacts();
     createNewContactMessage();
